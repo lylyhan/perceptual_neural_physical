49a998f36060958e71036a3ad5d532bbb9c092da
lylyhan <lilyhan0602@gmail.com>
2022-09-27 18:53:44 +0200

    physical

---
+++ icassp23/01_generate_audio.py
@@ -0,0 +1,83 @@
+import numpy as np
+import pandas
+import random
+import sys
+
+from ..pnp.physical import ftm  #not sure if this will work
+import soundfile as sf
+import os
+
+#define dataset path
+out_path = "/home/han/data/drum_data/"
+
+
+df_train = pandas.read_csv("./annotations/train_param_v2.csv") 
+df_val = pandas.read_csv("./annotations/val_param_v2.csv") 
+df_test = pandas.read_csv("./annotations/test_param_v2.csv") 
+
+print("finish loading annotations!")
+
+val_params = df_val.values
+train_params = df_train.values
+test_params = df_test.values
+
+n_samp_val,n_param_val = val_params.shape
+n_samp_test,n_param_val = test_params.shape
+n_samp_train,n_param_val = train_params.shape
+
+x1 = 0.4
+x2 = 0.4
+h = 0.03
+l0 = np.pi
+
+mode = 10
+sr = 22050
+
+print("begin making dataset!")
+
+path_out = os.path.join(out_path,"val")
+
+if not os.path.exists(path_out):
+    os.makedirs(path_out)       
+for i in range(n_samp_val):
+    omega,tau,p,D,alpha = val_params[i,1:-1]
+    y = ftm.getsounds_imp_linear_nonorm(mode,mode,x1,x2,h,tau,omega,p,D,l0,alpha,sr)
+    y = y/ max(y)
+    filename = os.path.join(path_out,str(val_params[i,0])+"_sound.wav")
+    sf.write(filename, y, sr)
+
+
+print("finished validation set!!")
+
+
+path_out = os.path.join(out_path,"test")
+if not os.path.exists(path_out):
+    os.makedirs(path_out)
+for i in range(n_samp_test):
+    omega,tau,p,D,alpha = test_params[i,1:-1]
+    y = ftm.getsounds_imp_linear_nonorm(mode,mode,x1,x2,h,tau,omega,p,D,l0,alpha,sr)
+    y = y/ max(y)
+    filename = os.path.join(path_out,str(test_params[i,0])+"_sound.wav")
+    sf.write(filename, y, sr)
+    
+    
+print("finished test set!!")
+
+path_out = os.path.join(out_path,"train")
+if not os.path.exists(path_out):
+    os.makedirs(path_out)
+for i in range(n_samp_train):
+    omega,tau,p,D,alpha = train_params[i,1:-1]
+    y = ftm.getsounds_imp_linear_nonorm(mode,mode,x1,x2,h,tau,omega,p,D,l0,alpha,sr)
+    y = y/ max(y)
+    filename = os.path.join(path_out,str(train_params[i,0])+"_sound.wav")
+    sf.write(filename, y, sr)
+    
+
+print("finished train set!!")
+
+
+
+
+
+
---
+++ pnp/physical/ftm.py
@@ -0,0 +1,45 @@
+"""
+This script implements Rabenstein's linear drum model
+drum model with normalized side length ratio/ side length, in impulse form
+"""
+import numpy as np
+import math
+
+
+def getsounds_imp_linear_nonorm(m1,m2,x1,x2,h,tau11,w11,p,D,l0,alpha_side,sr):
+    l2 = l0*alpha_side
+    beta_side = alpha_side + 1/alpha_side
+    S = l0/np.pi*((D*w11*alpha_side)**2 + (p*alpha_side/tau11)**2)**0.25
+    c_sq = (alpha_side*(1/beta_side-p**2*beta_side)/tau11**2 + alpha_side*w11**2*(1/beta_side-D**2*beta_side))*(l0/np.pi)**2
+    T = c_sq 
+    d1 = 2*(1-p*beta_side)/tau11
+    d3 = -2*p*alpha_side/tau11*(l0/np.pi)**2 
+
+    EI = S**4 
+
+    mu = np.arange(1,m1+1) #(0,1,2,..,m-1)
+    mu2 = np.arange(1,m2+1)
+    dur = 2**16
+    Ts = 1/sr
+    tau = 1/sr*np.arange(1,dur+1)
+
+    n = (mu*np.pi/l0)**2+(mu2*np.pi/l2)**2 #eta 
+    n2 = n**2 #(mu*np.pi/l0)**4+(mu2*np.pi/l2)**4 
+    K = np.sin(mu*math.pi*x1)*np.sin(mu2*math.pi*x2) #mu pi x / l
+
+    beta = EI*n2 + T*n #(m,1)
+    alpha = (d1-d3*n)/2 # nonlinear
+    omega = np.sqrt(np.abs(beta - alpha**2))
+    #correct partials
+    mode_corr = np.sum((omega/2/np.pi) <= sr/2) #convert omega to hz
+        
+    N = l0*l2/4
+    yi = h * np.sin(mu[:mode_corr] * np.pi * x1) * np.sin(mu2[:mode_corr] * np.pi * x2) / omega[:mode_corr]
+
+
+    time_steps = np.linspace(0,dur,dur)/sr
+    y = np.exp(-alpha[:mode_corr,None] * time_steps[None,:]) * np.sin(omega[:mode_corr,None] * time_steps[None,:]) 
+    y = yi[:,None] * y #(m,) * (m,dur)
+    y = np.sum(y * K[:mode_corr,None] / N,axis=0) #impulse response itself
+
+    return y
