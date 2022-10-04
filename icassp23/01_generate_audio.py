f9e39f2f6b1ed4cb7005ebbd02ad6a83c4ac182e
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2022-10-04 18:11:32 +0200

    call load_dataframe in 01

--- icassp23/01_generate_audio.py
+++ icassp23/01_generate_audio.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import pandas as pd
 import pnp_synth
@@ -13,23 +14,7 @@ id_end = int(sys.argv[3])
 
 csv_folder = os.path.join(os.path.dirname(__file__), "data")
 folds = ["test", "train", "val"]
-fold_dfs = {}
-
-for fold in folds:
-    csv_name = fold + "_param_log_v2.csv"
-    csv_path = os.path.join(csv_folder, csv_name)
-    fold_df = pd.read_csv(csv_path)
-    fold_dfs[fold] = fold_df
-    #make outpath dirs
-    path_out = os.path.join(out_path, fold)
-    if not os.path.exists(path_out):
-        os.makedirs(path_out)       
-
-
-full_df = pd.concat(fold_dfs.values()).sort_values(
-    by="ID", ignore_index=False
-)
-assert len(set(full_df["ID"])) == len(full_df)
+full_df = load_dataframe(folds)
 
 params = full_df.values
 n_samp = params.shape[0]
@@ -38,8 +23,8 @@ assert n_samp >= id_end > id_start >= 0 #nsamp is from 0 to 100k-1?
 
 for i in range(id_start, id_end):
     theta = params[i,3:-1]
-    fold = params[i,-1]
-    id = params[i,2]
+    fold = full_df["fold"].iloc[i]
+    id = full_df["ID"].iloc[i]
     y = pnp_synth.ftm.rectangular_drum(theta, **ftm.constants)
-    filename = os.path.join(out_path, fold, str(id)+"_sound.wav")
+    filename = os.path.join(out_path, fold, str(id) + "_sound.wav")
     sf.write(filename, y, ftm.constants["sr"])
