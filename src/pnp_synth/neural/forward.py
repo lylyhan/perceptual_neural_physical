0c9feb91b0744c33666f97a36ee407ca4be8a0a5
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2022-10-04 21:43:46 +0200

    update neural/forward

--- src/pnp_synth/neural/forward.py
+++ src/pnp_synth/neural/forward.py
@@ -19,17 +19,17 @@ def scale_theta(full_df):
     return nus, scaler
 
 
-def inverse_scale(nus, scaler):
+def inverse_scale(nu, scaler):
     "Apply inverse scaling theta = nu * (theta_max - theta_min) + theta_min"
     # NB: we use an explicit formula instead of scaler.inverse_transform
     # so as to preserve PyTorch differentiability.
     theta_max = torch.tensor(scaler.data_max_)
     theta_min = torch.tensor(scaler.data_min_)
-    thetas = nus * (theta_max - theta_min) + theta_min
-    return thetas
+    theta = nu * (theta_max - theta_min) + theta_min
+    return theta
 
 
-def pnp_forward(Phi, g, scaler, nu):
+def pnp_forward(nu, Phi, g, scaler):
     """
     Computes S = (Phi o g o h^{-1})(nu) = (Phi o g)(theta) = Phi(x), given:
     1. a scaler h
@@ -37,7 +37,7 @@ def pnp_forward(Phi, g, scaler, nu):
     3. a perceptual representation Phi
     """
     # Inverse parameter scaling
-    theta = inverse_scale(nus, scaler)
+    theta = inverse_scale(nu, scaler)
 
     # Synthesis
     x = g(theta)
