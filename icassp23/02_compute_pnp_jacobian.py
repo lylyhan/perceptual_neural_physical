9d6854fe31efca8b4e7210d6a0e023c4f962e9e3
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2022-10-04 18:34:17 +0200

    move functions to module

--- icassp23/02_compute_pnp_jacobian.py
+++ icassp23/02_compute_pnp_jacobian.py
@@ -20,73 +20,14 @@ import torch
 save_dir = "/scratch/vl1019/icassp23_data"
 folds = ["train", "test", "val"]
 
-jtfs_params = dict(
-    J = 14, # scattering scale ~ 1000 ms
-    shape = (2**17,), # 2**16 of zero padding plus 2**16 of signal
-    Q = 12, # number of filters per octave
-    T = 2**13, # local temporal averaging
-    F = 2, # local frequential averaging
-    max_pad_factor=1, # temporal padding cannot be greater than 1x support
-    max_pad_factor_fr=1, # frequential padding cannot be greater than 1x support
-    average = True, # average in time
-    average_fr = True, # average in frequency
-)
-
-
-def icassp23_synth(theta):
-    """Drum synthesizer, based on the Functional Transformation Method (FTM).
-    We apply 2**16 samples of zero padding (~3 seconds) on the left."""
-    x = pnp_synth.ftm.rectangular_drum(theta, **pnp_synth.ftm.constants)
-    padding = (pnp_synth.ftm.constants["dur"], 0)
-    x_padded = torch.nn.functional.pad(x, padding, mode='constant', value=0)
-    return x_padded
-
-
-def icassp23_perceptual(Phi, x):
-    # Sx is a tensor with shape (1, n_paths, n_time_frames)
-    Sx = Phi(x)
-
-    # remove leading singleton dimension and unpad
-    Sx_unpadded = Sx[0, :, Sx.shape[-1]:]
-
-    # flatten to shape (n_paths * n_time_frames,)
-    Sx_flattened = Sx_unpadded.flatten()
-
-    # apply "stable" log transformation
-    log_Sx = log1p(Sx)
-
-    return log_Sx
-
-
-def icassp23_pnp_forward_factory(scaler, jtfs_params):
-    """
-    Computes S = (Phi o g o h^{-1})(nu) = (Phi o g)(theta) = Phi(x), given:
-    1. a MinMax scaler h
-    2. an FTM synthesizer g
-    3. a JTFS representation Phi
-    """
-    # Instantiate Joint-Time Frequency Scattering (JTFS) operator
-    jtfs_operator = TimeFrequencyScattering1D(**jtfs_params)
-
-    perceptual_operator = functools.partial(
-        icassp23_perceptual, Phi=jtfs_operator)
-
-    return functools.partial(
-        pnp_synth.pnp_forward,
-        Phi=jtfs_operator,
-        g=icassp23_synth,
-        scaler=scaler)
-
-
 # Print header
 start_time = int(time.time())
 print(str(datetime.datetime.now()) + " Start.")
 print(__doc__ + "\n")
 out_path_jtfs = sys.argv[1]
 out_path_grad = sys.argv[2]
-csv_path = sys.argv[3]
-id_start = int(sys.argv[4])
-id_end = int(sys.argv[5])
+id_start = int(sys.argv[3])
+id_end = int(sys.argv[4])
 print("Command-line arguments:\n" + "\n".join(sys.argv[1:]) + "\n")
 
 for module in [kymatio, np, pd, sklearn, torch]:
@@ -108,12 +49,12 @@ assert n_samples > id_end > id_start + 1 > 0  # id is between 0 and (100k-1)
 nus, scaler = scale_theta(full_df)
 
 # Define the forward PNP operator.
-icassp23_pnp_forward = icassp23_pnp_forward_factory(scaler, jtfs_params)
+S_from_nu = icassp23.pnp_forward_factory(scaler, jtfs_params)
 
 # Define the associated Jacobian operator.
 # NB: jacfwd is faster than reverse-mode autodiff here because the input
 # is low-dimensional (5) whereas the output is high-dimensional (~1e4)
-icassp23_jacobian = functorch.jacfwd(icassp23_pnp_forward)
+nabla = functorch.jacfwd(pnp_forward)
 
 
 torch.autograd.set_detect_anomaly(True)
@@ -122,7 +63,7 @@ for i in range(id_start, id_end):
     nu = torch.tensor(nus[i, :], requires_grad=True)
     fold = full_df["fold"].iloc[i]
     id = full_df["ID"].iloc[i]
-    S = icassp23_pnp_forward(nu)
+    S = S_from_nu(nu)
 
     # Compute Jacobian: d(S) / d(nu)
     jacobian = icassp23_jacobian(nu)
--- icassp23/icassp23.py
+++ icassp23/icassp23.py
@@ -1,3 +1,16 @@
+jtfs_params = dict(
+    J = 14, # scattering scale ~ 1000 ms
+    shape = (2**17,), # 2**16 of zero padding plus 2**16 of signal
+    Q = 12, # number of filters per octave
+    T = 2**13, # local temporal averaging
+    F = 2, # local frequential averaging
+    max_pad_factor=1, # temporal padding cannot be greater than 1x support
+    max_pad_factor_fr=1, # frequential padding cannot be greater than 1x support
+    average = True, # average in time
+    average_fr = True, # average in frequency
+)
+
+
 def load_dataframe(folds):
     "Load DataFrame corresponding to the entire dataset (100k drum sounds)."
     fold_dfs = {}
@@ -11,3 +24,50 @@ def load_dataframe(folds):
     full_df = full_df.sort_values(by="ID", ignore_index=False)
     assert len(set(full_df["ID"])) == len(full_df)
     return full_df
+
+
+def x_from_theta(theta):
+    """Drum synthesizer, based on the Functional Transformation Method (FTM).
+    We apply 2**16 samples of zero padding (~3 seconds) on the left."""
+    x = pnp_synth.ftm.rectangular_drum(theta, **pnp_synth.ftm.constants)
+    padding = (pnp_synth.ftm.constants["dur"], 0)
+    x_padded = torch.nn.functional.pad(x, padding, mode='constant', value=0)
+    return x_padded
+
+
+def S_from_x(jtfs_operator, x):
+    """
+    Computes log-compressed Joint-Time Frequency Scattering.
+    """
+    # Sx is a tensor with shape (1, n_paths, n_time_frames)
+    Sx = jtfs_operator(x)
+
+    # remove leading singleton dimension and unpad
+    Sx_unpadded = Sx[0, :, Sx.shape[-1]:]
+
+    # flatten to shape (n_paths * n_time_frames,)
+    Sx_flattened = Sx_unpadded.flatten()
+
+    # apply "stable" log transformation
+    log1p_Sx = log1p(Sx)
+
+    return log1p_Sx
+
+
+def pnp_forward_factory(scaler, jtfs_params):
+    """
+    Computes S = (Phi o g o h^{-1})(nu) = (Phi o g)(theta) = Phi(x), given:
+    1. a MinMax scaler h
+    2. an FTM synthesizer g
+    3. a JTFS representation Phi
+    """
+    # Instantiate Joint-Time Frequency Scattering (JTFS) operator
+    jtfs_operator = TimeFrequencyScattering1D(**jtfs_params)
+
+    Phi = functools.partial(S_from_theta, jtfs_operator=jtfs_operator)
+
+    return functools.partial(
+        pnp_synth.pnp_forward,
+        Phi=S_from_x,
+        g=x_from_theta,
+        scaler=scaler)
