a874a5004a67113a18c913cea51b6c66b547ded6
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2022-10-06 19:10:40 +0200

    update 01 sbatch

--- icassp23/sbatch/01_generate_audio.py
+++ icassp23/sbatch/01_generate_audio.py
@@ -1,13 +1,6 @@
 import os
-import sys
-
-sys.path.append("../src")
-
 
 # Define constants.
-id_max = 100000
-n_threads = 10
-n_per_th = id_max // n_threads
 script_name = os.path.basename(__file__)
 script_path = os.path.abspath(os.path.join("..", script_name))
 save_dir = "/scratch/vl1019/icassp23_data"
@@ -16,66 +9,36 @@ save_dir = "/scratch/vl1019/icassp23_data"
 sbatch_dir = os.path.join(".", os.path.basename(__file__)[:-3])
 os.makedirs(sbatch_dir, exist_ok=True)
 
+job_name = "_".join([script_name[:2], "thread-" + str(n_thread)])
+file_name = job_name + ".sbatch"
+file_path = os.path.join(sbatch_dir, file_name)
 
-for n_thread in range(n_threads):
-
-    job_name = "_".join([script_name[:2], "thread-" + str(n_thread)])
-    file_name = job_name + ".sbatch"
-    file_path = os.path.join(sbatch_dir, file_name)
-
-    # Generate file.
-    with open(file_path, "w") as f:
-        id_start = n_thread * n_per_th
-        id_end = (n_thread + 1) * n_per_th
-        if n_thread == n_threads - 1:
-            id_end = max(id_end, id_max)
-        cmd_args = [script_path, save_dir, str(id_start), str(id_end)]
-
-        f.write("#!/bin/bash\n")
-        f.write("\n")
-        f.write("#BATCH --job-name=" + script_name + "\n")
-        f.write("#SBATCH --nodes=1\n")
-        f.write("#SBATCH --tasks-per-node=1\n")
-        f.write("#SBATCH --cpus-per-task=4\n")
-        f.write("#SBATCH --time=10:00:00\n")
-        f.write("#SBATCH --mem=8GB\n")
-        f.write("#SBATCH --gres=gpu:1\n")
-        f.write("#SBATCH --output=" + job_name + "_%j.out\n")
-        f.write("\n")
-        f.write("module purge\n")
-        f.write("module load cuda/11.6.2\n")
-        f.write("module load ffmpeg/4.2.4\n")
-        f.write("\n")
-        f.write(" ".join([
-            "singularity exec",
-            "--overlay /scratch/vl1019/overlay-50G-10M.ext3:ro",
-            "/scratch/work/public/singularity/cuda11.0-cudnn8-devel-ubuntu18.04.sif",
-            "/bin/bash",
-            "-c",
-            "\"source",
-                "/scratch/vl1019/env.sh;",
-                "python"] + cmd_args) + "\"\n")
-        f.write("\n")
-
-
-# Open shell file.
-file_path = os.path.join(sbatch_dir, script_name[:2] + ".sh")
-
+# Generate file.
 with open(file_path, "w") as f:
-    # Print header.
-    f.write("# This shell script generates audio from virtual drum shapes.\n")
+    cmd_args = [script_path, save_dir]
+
+    f.write("#!/bin/bash\n")
     f.write("\n")
-
-    # Loop over folds: training and validation.
-    for n_thread in range(n_threads):
-        # Define job name.
-        job_name = "_".join([script_name[:2], "thread-" + str(n_thread)])
-        sbatch_str = "sbatch " + job_name + ".sbatch"
-        # Write SBATCH command to shell file.
-        f.write(sbatch_str + "\n")
-
-# Grant permission to execute the shell file.
-# https://stackoverflow.com/a/30463972
-mode = os.stat(file_path).st_mode
-mode |= (mode & 0o444) >> 2
-os.chmod(file_path, mode)
+    f.write("#BATCH --job-name=" + script_name + "\n")
+    f.write("#SBATCH --nodes=1\n")
+    f.write("#SBATCH --tasks-per-node=1\n")
+    f.write("#SBATCH --cpus-per-task=4\n")
+    f.write("#SBATCH --time=24:00:00\n")
+    f.write("#SBATCH --mem=8GB\n")
+    f.write("#SBATCH --gres=gpu:1\n")
+    f.write("#SBATCH --output=" + job_name + "_%j.out\n")
+    f.write("\n")
+    f.write("module purge\n")
+    f.write("module load cuda/11.6.2\n")
+    f.write("module load ffmpeg/4.2.4\n")
+    f.write("\n")
+    f.write(" ".join([
+        "singularity exec",
+        "--overlay /scratch/vl1019/overlay-50G-10M.ext3:ro",
+        "/scratch/work/public/singularity/cuda11.0-cudnn8-devel-ubuntu18.04.sif",
+        "/bin/bash",
+        "-c",
+        "\"source",
+            "/scratch/vl1019/env.sh;",
+            "python"] + cmd_args) + "\"\n")
+    f.write("\n")
