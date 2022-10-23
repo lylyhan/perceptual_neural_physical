58116eb2405a210a51e209d0102afbfec5ccdbbb
Vincent Lostanlen <vincent.lostanlen@ls2n.fr>
2022-10-23 11:51:35 +0200

    07b 08b in sbatch

--- icassp23/sbatch/07b_eval_effnet_ploss_novol.py
+++ icassp23/sbatch/07b_eval_effnet_ploss_novol.py
@@ -6,7 +6,7 @@ script_path = os.path.abspath(os.path.join("..", script_name)) + ".py"
 save_dir = "/scratch/vl1019/icassp23_data"
 n_inits = 10
 batch_size = 256
-ckpt_path = "last.ckpt"
+ckpt_path = "best.ckpt"
 
 # Create folder.
 sbatch_dir = os.path.join(".", script_name)
@@ -22,7 +22,7 @@ for init_id in range(n_inits):
     with open(file_path, "w") as f:
         cmd_args = [
             os.path.abspath(os.path.join("..",
-                "08_train_effnet_pnploss_novol_adaptive_brake1.py")),
+                "07_train_effnet_pnploss_novol.py")),
             save_dir, str(init_id), str(batch_size), ckpt_path]
 
         f.write("#!/bin/bash\n")
