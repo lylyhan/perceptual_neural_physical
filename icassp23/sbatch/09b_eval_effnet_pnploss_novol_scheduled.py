b30af0b3a9bed01434a782c7a914d8f66fb46444
lylyhan <lilyhan0602@gmail.com>
2022-10-26 09:06:14 +0200

    fix sbatch

--- icassp23/sbatch/09b_eval_effnet_pnploss_novol_scheduled.py
+++ icassp23/sbatch/09b_eval_effnet_pnploss_novol_scheduled.py
@@ -22,7 +22,7 @@ for init_id in range(n_inits):
     with open(file_path, "w") as f:
         cmd_args = [
             os.path.abspath(os.path.join("..",
-                "09_train_effnet_pnploss_novol_scheduled.py")),
+                "09b_train_effnet_pnploss_novol_scheduled.py")),
             save_dir, str(init_id), str(batch_size), ckpt_path]
 
         f.write("#!/bin/bash\n")
