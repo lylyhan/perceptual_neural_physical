9c6af9087268e650c03b978132a6a57b86502828
lylyhan <lilyhan0602@gmail.com>
2023-04-16 22:42:33 +0200

    nominmax

---
+++ icassp23/03c_train_effnet_ploss_modal_nominmax.py
@@ -0,0 +1,176 @@
+"""
+This script trains an EfficientNet for sound matching of drum sounds
+with Parametric Loss (P-Loss) as its objective.
+"""
+from ast import Mod
+import datetime
+import joblib
+import nnAudio
+import numpy as np
+import os
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+import sklearn
+import sys
+import time
+import torch
+from pytorch_lightning import loggers as pl_loggers
+from datetime import timedelta
+
+import icassp23
+from pnp_synth.neural import cnn
+from pnp_synth import utils
+
+
+start_time = int(time.time())
+print(str(datetime.datetime.now()) + " Start.")
+print(__doc__ + "\n")
+save_dir = sys.argv[1]  # /home/han/data/
+init_id = sys.argv[2]
+batch_size = int(sys.argv[3])
+if len(sys.argv) < 5:
+    is_train = True
+else:
+    is_train = False
+    ckpt_path = sys.argv[4]
+
+print("Command-line arguments:\n" + "\n".join(sys.argv[1:]) + "\n")
+
+for module in [joblib, nnAudio, np, pl, sklearn, torch]:
+    print("{} version: {:s}".format(module.__name__, module.__version__))
+print("")
+sys.stdout.flush()
+
+data_dir = os.path.join(save_dir, "x")
+weight_dir = os.path.join(save_dir, "M")
+model_dir = os.path.join(save_dir, "f_W")
+cqt_dir = data_dir
+
+
+epoch_max = 70
+steps_per_epoch = icassp23.SAMPLES_PER_EPOCH / batch_size
+max_steps = steps_per_epoch * epoch_max
+# feature parameters
+Q = 12
+J = 10
+outdim = 5
+bn_var = 0.5
+cnn_type = "efficientnet"  # efficientnet / cnn.wav2shape
+loss_type = "ploss"  # spec / weighted_p / ploss
+weight_type = "None"  # novol / pnp / None
+logscale_theta = False
+synth_type = "ftm"
+minmax = False
+utils.logscale = logscale_theta 
+icassp23.logscale = logscale_theta
+utils.synth_type = synth_type
+assert utils.logscale == logscale_theta
+assert icassp23.logscale == logscale_theta
+
+
+
+if __name__ == "__main__":
+    print("Current device: ", torch.cuda.get_device_name(0))
+    torch.multiprocessing.set_start_method('spawn')
+    model_save_path = os.path.join(
+        model_dir,
+        "_".join(
+            [
+                cnn_type,
+                loss_type,
+                weight_type,
+                str(J),
+                str(Q),
+                "batch_size" + str(batch_size),
+                "bn_var" + str(bn_var),
+                "init-" + str(init_id),
+                "outdim-" + str(outdim),
+                "log-" + str(logscale_theta),
+                "minmax-" + str(minmax),
+            ]
+        ),
+    )
+    os.makedirs(model_save_path, exist_ok=True)
+    pred_path = os.path.join(model_save_path, "test_predictions.npy")
+
+    if minmax:
+        nus, scaler = icassp23.scale_theta()
+    else:
+        scaler = None
+    #no min max scaling
+    full_df = icassp23.load_fold(fold="full")
+
+   
+    # initialize dataset
+    dataset = cnn.DrumDataModule(
+        batch_size=batch_size,
+        data_dir=data_dir,  # path to hdf5 files
+        cqt_dir=cqt_dir,
+        df=full_df,
+        weight_dir=weight_dir,  # path to gradient folders
+        weight_type=weight_type,  # novol, pnp
+        feature="cqt",
+        J=J,
+        Q=Q,
+        scaler=scaler,
+        num_workers=0
+    )
+
+    print(str(datetime.datetime.now()) + " Finished initializing dataset")
+    # initialize model, designate loss function
+    if cnn_type == "cnn.wav2shape":
+        model = cnn.wav2shape(
+            in_channels=1, bin_per_oct=Q, outdim=outdim, loss=loss_type, scaler=scaler
+        )
+    elif cnn_type == "efficientnet":
+        model = cnn.EffNet(in_channels=1, outdim=outdim, loss=loss_type, scaler=scaler, var=bn_var, save_path=pred_path, minmax=False)
+    print(str(datetime.datetime.now()) + " Finished initializing model")
+
+    # initialize checkpoint methods
+    checkpoint_cb = ModelCheckpoint(
+        dirpath=model_save_path,
+        monitor="val_loss",
+        save_last=True,
+        filename= "best", #"ckpt-{epoch:02d}-{val_loss:.2f}",
+        save_weights_only=False,
+    )
+    tb_logger = pl_loggers.TensorBoardLogger(save_dir=os.path.join(model_save_path,"logs"))
+
+    # initialize trainer, declare training parameters, possiibly in neural/cnn.py
+    trainer = pl.Trainer(
+        accelerator="gpu",
+        devices=-1,
+        auto_select_gpus=True,
+        max_epochs=epoch_max,
+        max_steps=max_steps,
+        limit_train_batches=steps_per_epoch,  # if integer than it's #steps per epoch, if float then it's percentage
+        limit_val_batches=1.0,
+        limit_test_batches=1.0,
+        callbacks=[checkpoint_cb],
+        enable_progress_bar=True,
+        logger=tb_logger,
+        max_time=timedelta(hours=12)
+    )
+    # train
+    if is_train:
+        print("Training ...")
+        trainer.fit(model, dataset)
+    else:
+        print("Skipped Training, loading model")
+        model = model.load_from_checkpoint(os.path.join(model_save_path, ckpt_path),in_channels=1, outdim=outdim, loss=loss_type, scaler=scaler, var=bn_var, save_path=pred_path, minmax=False)
+
+    test_loss = trainer.test(model, dataset, verbose=False)
+    print("Model saved at: {}".format(model_save_path))
+    print("Average test loss: {}".format(test_loss))
+    print("\n")
+
+    # Print elapsed time.
+    print(str(datetime.datetime.now()) + " Success.")
+    elapsed_time = time.time() - int(start_time)
+    elapsed_hours = int(elapsed_time / (60 * 60))
+    elapsed_minutes = int((elapsed_time % (60 * 60)) / 60)
+    elapsed_seconds = elapsed_time % 60.0
+    elapsed_str = "{:>02}:{:>02}:{:>05.2f}".format(
+        elapsed_hours, elapsed_minutes, elapsed_seconds
+    )
+    print("Total elapsed time: " + elapsed_str + ".")
---
+++ icassp23/sbatch/03c_train_effnet_ploss_modal_nominmax.py
@@ -0,0 +1,38 @@
+import os
+
+# Define constants.
+script_name = os.path.basename(__file__)[:-3] # remove .py
+script_path = os.path.abspath(os.path.join("..", script_name)) + ".py"
+save_dir = "/gpfswork/rech/rwb/ufg99no/data/icassp23_data"
+n_inits = 1
+batch_size = 256
+
+
+for init_id in range(n_inits):
+    job_name = "_".join([script_name, "init-" + str(init_id)])
+
+    file_name = job_name + ".sbatch"
+    file_path = os.path.join(".", file_name)
+
+    # Generate file.
+    with open(file_path, "w") as f:
+        cmd_args = [script_path, save_dir, str(init_id), str(batch_size)]
+
+        f.write("#!/bin/bash\n")
+        f.write("\n")
+        f.write("#BATCH --job-name=" + script_name + "\n")
+        f.write("#SBATCH --nodes=1\n")
+        f.write("#SBATCH --tasks-per-node=1\n")
+        f.write("#SBATCH --cpus-per-task=10\n")
+        f.write("#SBATCH --time=20:00:00\n")
+        f.write("#SBATCH --gres=gpu:1\n")
+        f.write("#SBTACH -A aej@v100\n")
+        f.write("#SBATCH --output=" + job_name + "_%j.out\n")
+        f.write("\n")
+        f.write("module purge\n")
+        f.write("module load pytorch-gpu/py3/1.12.1\n")
+        f.write("\n")
+        f.write(" ".join(["python"] + cmd_args) + "\"\n")
+        f.write("\n")
+
+
