0977a2c81bb34c2cd9c7ac8e0dcf36275e72c2d9
lylyhan <lilyhan0602@gmail.com>
2024-07-18 16:25:34 +0200

    merge

--- icassp25/06_eval_grad.py
+++ icassp25/06_eval_grad.py
@@ -100,7 +100,7 @@ def eval_smooth(prev_model, model, nbatch, num_pts=1):
 # load data
 _, scaler = icassp25.scale_theta(logscale=1)
 dataset = cnn.DrumDataModule(
-        batch_size=64,
+        batch_size=batch_size,
         data_dir=os.path.join(data_dir, "x"),  # path to hdf5 files
         cqt_dir=os.path.join(data_dir, "x"),
         df=full_df,
@@ -129,10 +129,10 @@ LMA = {
         'damping': "id"
     }
 
-steps_per_epoch = 100 #icassp25.SAMPLES_PER_EPOCH / batch_size
+steps_per_epoch = icassp25.SAMPLES_PER_EPOCH / batch_size
 lr = 1e-3
 log_interval = 1 # frequency every number of batches to log gradient norm/smoothness 
-mu = 1e-10
+mu = 1e-20
 
 model = cnn.EffNet(in_channels=1, outdim=outdim, loss=loss_type, eff_type=eff_type, 
                        scaler=scaler, LMA=LMA, steps_per_epoch=steps_per_epoch,
